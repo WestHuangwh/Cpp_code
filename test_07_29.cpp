2a3a38dd36598e58675cbe68121edd20f3508f29
WestHuangwh <1502114880@qq.com>
2023-07-30 22:55:45 +0800

    更改一次

--- test_07_29.cpp
+++ test_07_29.cpp
@@ -32,28 +32,145 @@
 //
 
 
+//#include<iostream>
+//using namespace std;
+//
+//class Person
+//{
+//public:
+//	void PrintPersonInfo();//声明
+//private:
+//	char _name[20];//这是声明
+//	char _gender[3];
+//	int _age;
+//};
+//
+////这里需要指定PrintPersonInfo是属于Person这个类域
+//void Person::PrintPersonInfo()//函数定义
+//{
+//	cout << _name << " " << _gender << " " << _age << endl;
+//
+//}
+//int main()
+//{
+//
+//	return 0;
+//}
+
+
+//#include<iostream>
+//
+//class A
+//{
+//public:
+//	void f();
+//private:
+//	char a;
+//	int _a;
+//};
+//
+//class A2
+//{
+//public:
+//	void f2() {};//成员函数不占用类的空间
+//};
+//
+//class A3//空类占一个字节的大小
+//{};
+//
+//int main()
+//{
+//	std::cout << sizeof(A) << std::endl;
+//	std::cout << sizeof(A2) << std::endl;
+//	std::cout << sizeof(A3) << std::endl;
+//
+//	//没有成员变量的类对象，给一个字节的大小，占位且不存储实际数据，标识对象存在
+//	A2 aa2;
+//	A3 aa3;
+//	std::cout << &aa2 << std::endl;
+//	std::cout << &aa3 << std::endl;
+//
+//	return 0;
+//}
+
+
+
+
+////驼峰法命名规则：
+////a、函数名、类名等所有单词首字母大写，如DateMgr
+////b、变量首字母小写，后面单词首字母大写，如dateMgr
+////c、成员变量，首字母前面加_   如_dateMgr
+//
+////this指针存在哪里？ --栈，因为它是一个形参
+////优化：取决于编译器
+////vs下面传递this指针，是通过ecx寄存器传递的，这样this访问变量提高效率
+//#include<iostream>
+//using namespace std;
+//
+//class Date
+//{
+//public:
+//	void Init(int year,int month,int day)
+//	{
+//		/*this->*/_year = year;//this指针在这里没有显式实现，编译器的使用是默认加上this指针的
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//
+//private:
+//	int _year;//年
+//	int _month;//月
+//	int _day;//日
+//};
+//
+//int main()
+//{
+//	Date s1;
+//	s1.Init(2022, 7, 30);
+//	
+//	Date s2;
+//	s2.Init(2022, 7, 31);
+//
+//	s1.Print();
+//	s2.Print();
+//	return 0;
+//}
+
+
+
 #include<iostream>
 using namespace std;
 
-class Person
+class Date
 {
 public:
-	void PrintPersonInfo();
+	//构造函数
+	Date(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+
+	}
+
+	void Print()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+
 private:
-	char _name[20];
-	char _gender[3];
-	int _age;
+	int _year;//年
+	int _month;//月
+	int _day;//日
 };
 
-//这里需要指定PrintPersonInfo是属于Person这个类域
-void Person::PrintPersonInfo()
-{
-	cout << _name << " " << _gender << " " << _age << endl;
-
-}
 int main()
 {
-
-
+	Date s1(2022, 7, 30);
 	return 0;
 }
